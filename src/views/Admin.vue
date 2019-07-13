263bbf2a61786d0bbd14cc179a41d60f8c78c4b6
Niv Bekelman <nivb52@gmail.com>
2019-07-14 02:02:49 +0300

    comment admin list to avoid errors

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -4,7 +4,7 @@
 		<div class="add-filter">
 			<div class="add-yachts">
 				<!-- <admin-yacht-list @emitRemoveYacht="removeYacht" :yachts="yachts"></admin-yacht-list> -->
-				<AdminYachtTable :yachts="yachts" />
+				<AdminYachtTable :yachts="yachts" @removeYacht="removeYacht" />
 			</div>
 		</div>
 	</section>
@@ -12,7 +12,7 @@
 </template>
 
 <script>
-import AdminYachtList from "@/components/UserAdmin/AdminYachtList.vue";
+// import AdminYachtList from "@/components/UserAdmin/AdminYachtList.vue";
 import AdminYachtTable from "@/components/UserAdmin/AdminYachtTable";
 
 export default {
@@ -34,7 +34,7 @@ export default {
 		}
 	},
 	components: {
-		AdminYachtList,
+		// AdminYachtList,
 		AdminYachtTable
 	}
 };
