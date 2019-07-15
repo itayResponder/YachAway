9663f7c5090b3e915f0c1cfca6ffff3235d1c4ad
Niv Bekelman <nivb52@gmail.com>
2019-07-15 20:08:35 +0300

    stule disabled dates

--- src/components/general/CalendarShow.vue
+++ src/components/general/CalendarShow.vue
@@ -34,16 +34,32 @@ export default {
 
 <style>
 .vl-calendar {
-	background: #ffffff;
+	background: #ffffff !important;
+}
+.vl-calendar-month__week-day {
+	color: #4d9989 !important;
 }
 .vl-calendar-month__day:hover,
-.vl-calendar-month__day > selected--first,
-.vl-calendar-month__day > selected--last,
+.vl-calendar-month__day > .selected--first,
+.vl-calendar-month__day > .selected--last,
 .vl-flex .selected {
 	background: #4d9989 !important;
 	font-weight: 600 !important;
 }
 .enable-single-date {
 	background: #222b29 !important;
+}
+.disabled {
+	background: repeating-linear-gradient(
+		-45deg,
+		rgb(255, 255, 255),
+		rgb(255, 255, 255) 3px,
+		rgb(235, 235, 235) 3px,
+		rgb(235, 235, 235) 4px
+	);
+	/* color: rgb(0, 0, 0); */
+	border: 2px solid rgb(255, 255, 255) !important;
+	border-radius: 7px !important;
+	padding: 0px !important;
 }
 </style>
