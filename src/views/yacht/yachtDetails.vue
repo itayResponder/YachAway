cb1cb67308c40ed2f139c40317d8293cfc7ef767
Niv Bekelman <nivb52@gmail.com>
2019-07-16 20:50:55 +0300

    RESERVATION BOX

--- src/views/yacht/yachtDetails.vue
+++ src/views/yacht/yachtDetails.vue
@@ -1,32 +1,38 @@
 <template>
+	<!-- IT MAYBE NEED TO USE TILES AND NOT GRID FOR RESPONSIVE -->
+	<div class="container grid">
 
-	<div class="content">
-		<a target="_blank" href="https://demo07.gethomey.io/listing/large-and-modern-bedroom/">
+		<reservationBox /> <!-- is slots possible ?? -->
+		<div></div>
+		<div class="content ">
+			<a target="_blank" href="https://demo07.gethomey.io/listing/large-and-modern-bedroom/">
 
-			<img src="@/assets/img/temp/details-yacht-pics.jpg" />
-		</a>
-		<a target="_blank" href="https://demo03.gethomey.io/listing/large-and-modern-bedroom/">
-			<img src="@/assets/img/temp/details-top-bnb.jpg" />
-		</a>
+				<img src="@/assets/img/temp/details-yacht-pics.jpg" />
+				<!-- <figure class="image is-4by3">
+				<img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+			</figure> -->
+			</a>
+			<a target="_blank" href="https://demo03.gethomey.io/listing/large-and-modern-bedroom/">
+				<img src="@/assets/img/temp/details-top-bnb.jpg" />
+			</a>
 
-		<img v-show="toggleDesc" @click="toggleDesc=!toggleDesc" src="@/assets/img/temp/details-descreption-bnb.jpg" />
-		<img v-show="!toggleDesc" @click="toggleDesc=!toggleDesc" src="@/assets/img/temp/details-descreption.jpg" />
+			<img v-show="toggleDesc" @click="toggleDesc=!toggleDesc" src="@/assets/img/temp/details-descreption-bnb.jpg" />
+			<img v-show="!toggleDesc" @click="toggleDesc=!toggleDesc" src="@/assets/img/temp/details-descreption.jpg" />
 
-		<img v-show="toggleFacility" @click="toggleFacility=!toggleFacility" src="@/assets/img/temp/details-popular-facility-bnb.jpg" />
-		<img v-show="!toggleFacility" @click="toggleFacility=!toggleFacility" src="@/assets/img/temp/details-popular-facility.jpg" />
-		<datePicker />
-		<showFreeDates />
+			<img v-show="toggleFacility" @click="toggleFacility=!toggleFacility" src="@/assets/img/temp/details-popular-facility-bnb.jpg" />
+			<img v-show="!toggleFacility" @click="toggleFacility=!toggleFacility" src="@/assets/img/temp/details-popular-facility.jpg" />
+			<calendarShow />
 
-		<previewReview />
-		<img src="@/assets/img/temp/details-reviews.jpg" />
+			<previewReview />
+			<img src="@/assets/img/temp/details-reviews.jpg" />
+		</div>
 	</div>
-
 </template>
 
 <script>
-import datePicker from "@/components/general/DatePicker";
-import showFreeDates from "@/components/general/CalendarShow";
+import calendarShow from "@/components/general/CalendarShow";
 import previewReview from "@/components/Yachts/PreviewReview";
+import reservationBox from "@/components/Yachts/ReservationBox";
 export default {
 	data() {
 		return {
@@ -36,9 +42,9 @@ export default {
 		};
 	},
 	components: {
-		datePicker,
-		showFreeDates,
-		previewReview
+		calendarShow,
+		previewReview,
+		reservationBox
 	}
 };
 </script>
