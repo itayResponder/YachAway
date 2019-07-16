2c4c5a027c0b212d4fb2c9ff0e50a43524c99dec
Niv Bekelman <nivb52@gmail.com>
2019-07-16 20:52:38 +0300

    style import

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,34 @@
 <template>
-  <div id="app">
-    <my-header :loggedInUser="getLoggedInUser" v-if="!headerType"></my-header>
-    <router-view></router-view>
-  </div>
+	<div id="app">
+		<my-header :loggedInUser="getLoggedInUser" v-if="!headerType"></my-header>
+		<router-view></router-view>
+	</div>
 </template>
 
 <script>
 import MyHeader from "./components/MyHeader.vue";
 export default {
-  name: "App",
-  data() {
-    return {};
-  },
-  created() {
-    this.$store.dispatch("loadYachts");
-  },
-  components: {
-    MyHeader
-  },
+	name: "App",
+	data() {
+		return {};
+	},
+	created() {
+		this.$store.dispatch("loadYachts");
+	},
+	components: {
+		MyHeader
+	},
 
-  computed: {
-    headerType() {
-      return this.$route.name === "Home";
-    },
-    getLoggedInUser() {
-		const user =  this.$store.getters.userLoggedIn;
-	  console.log("checking comp", user);
-	  return user
-    }
-  }
+	computed: {
+		headerType() {
+			return this.$route.name === "Home";
+		},
+		getLoggedInUser() {
+			const user = this.$store.getters.userLoggedIn;
+			console.log("checking comp", user);
+			return user;
+		}
+	}
 };
 </script>
 
@@ -36,43 +36,44 @@ export default {
 // Import Bulma's core
 @import "~bulma/sass/utilities/_all";
 @import url("https://fonts.googleapis.com/css?family=EB+Garamond&display=swap");
-// @import '../assets/css/main.css';
+// @import "assets/css/helpers.css";
+@import "assets/css/main.css";
 
 // Set your colors
 $primary: #8c67ef;
 
 #app {
-  // font-family: "Avenir", Helvetica, Arial, sans-serif;
-  h1,
-  h2,
-  h3,
-  h4,
-  h5,
-  h6,
-  .is-1,
-  .is-2 {
-    font-family: "EB Garamond", serif;
-  }
+	// font-family: "Avenir", Helvetica, Arial, sans-serif;
+	h1,
+	h2,
+	h3,
+	h4,
+	h5,
+	h6,
+	.is-1,
+	.is-2 {
+		font-family: "EB Garamond", serif;
+	}
 
-  body {
-    font-family: BlinkMacSystemFont, -apple-system, "Segoe UI", "Roboto",
-      "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans",
-      "Helvetica Neue", "Helvetica", "Arial", sans-serif !important;
-  }
+	body {
+		font-family: BlinkMacSystemFont, -apple-system, "Segoe UI", "Roboto",
+			"Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans",
+			"Helvetica Neue", "Helvetica", "Arial", sans-serif !important;
+	}
 
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
 }
 #nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+	padding: 30px;
+	a {
+		font-weight: bold;
+		color: #2c3e50;
+		&.router-link-exact-active {
+			color: #42b983;
+		}
+	}
 }
 </style>
