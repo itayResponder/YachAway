458bde25238bff9927dc5d0220698fbfcb26b3a5
Niv Bekelman <nivb52@gmail.com>
2019-07-17 12:20:00 +0300

    search google-autocomplete

---
+++ src/services/geocode.service.js
@@ -0,0 +1,36 @@
+export default {
+    getCityByLatLng,
+    getLatLngByAddress,
+    getPosition
+}
+
+const API_KEY = 'AIzaSyAQz_Zc9Ys9pFeNAYxOhagonVUGOyg_zlg';
+
+function getCityByLatLng(lat, lng) {
+    return fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${API_KEY}`)
+        .then(res => (res.json()))
+        .then(resData => {
+            const city = resData.results.find((components) => {
+                return components.types.includes("locality")
+            })
+            return city.formatted_address;
+        })
+        .catch((err) => {
+            console.log('there is a problam', err);
+        })
+}
+
+function getLatLngByAddress(address) {
+    return fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${API_KEY}`)
+        .then(res => (res.json()))
+        .then(resData => (resData.results[0].geometry.location))
+        .catch((err) => {
+            console.log('there is a problam', err);
+        })
+}
+
+function getPosition() {
+    return new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject)
+    })
+}
