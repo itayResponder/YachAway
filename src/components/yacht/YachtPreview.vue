63422f9576bb6916982c16fae044e5ffda6748d0
nivb52 <nivb52@gmail.com>
2019-07-22 12:23:49 +0300

    fix reviews css and img

--- src/components/yacht/YachtPreview.vue
+++ src/components/yacht/YachtPreview.vue
@@ -2,8 +2,10 @@
   <div class="columns is-12 is-mobile row-shadow yacht-list-preview-margin max-height">
     <div class="column is-one-third">
       <!-- image is-4by5 -->
+      <div class="container">
+
       <figure class="image img-wrap img-hover-zoom" style="overflow: hidden;">
-        <img :src="yacht.imgs[0]" class="img-boat" style="height:auto; object-fit: fill; overflow: hidden;" />
+        <img :src="yacht.imgs[0]" class="img-boat" style="object-fit: fill; overflow: hidden;" />
         <div v-if="loggedInUser">
           <div v-if="liked">
             <img
@@ -12,7 +14,7 @@
               src="@/assets/icons/heart-multiple-outline.svg"
               alt="you don't like this yacht yet"
               class="is-relative like"
-              style="height:50px; z-index: 10; float:right; left: 41%; top:-12.5rem; padding:10px;"
+              style="height:50px; z-index: 10; float:right; left: 40%; top:-12.5rem; padding:10px;"
             />
 
             <img
@@ -21,11 +23,13 @@
               src="@/assets/icons/heart-multiple.svg"
               alt="favorite yacht"
               class="is-relative like"
-              style="height:50px; z-index: 10; float:right; left: 41%; top:-12.5rem; padding:10px;"
+              style="height:50px; z-index: 10; float:right; left: 40%; top:-12.5rem; padding:10px;"
             />
           </div>
         </div>
       </figure>
+      </div>
+
       <div style="position:relative;"></div>
     </div>
     
@@ -33,7 +37,7 @@
     <!-- TODO : SHOW IT IN A BETTER WAY -->
     
     <!-- TEXT -->
-    <!-- <div class="column is-6 horiznal-shadow"> -->
+    <div class="column is-6 horiznal-shadow">
       <router-link :to="getUrlWithYachtId">
         <!-- <div class="media-content"> -->
           <!-- <div class="content"> -->
@@ -46,16 +50,16 @@
           <!-- <div class="content"> -->
           
           <!-- <div class="level-left level-item"> -->
-          <p>
-            <b>{{yacht.owner.name}}</b>
+          <p class=" has-text-grey">
+            {{yacht.owner.name}}
             <!-- THE FACILITES -->
           </p>
           <!-- </div> -->
 
           <!-- <div class="level-left is-hidden-mobile"> -->
           <p class="is-hidden-mobile">
-            <span class=" has-text-grey"> {{yacht.location.country}}</span>,
-            <span>{{yacht.location.city}}</span>
+            <b > {{yacht.location.country}}</b>,
+            <b>{{yacht.location.city}}</b>
           </p>
           <figure class="image is-48x48 margin-min">
             <img class="is-rounded" :src="yacht.owner.img" />
@@ -64,7 +68,7 @@
           <!-- </div> -->
         <!-- </nav> -->
       </router-link>
-    <!-- </div> -->
+    </div>
 
     <!-- REVIEWS AND PRICE -->
     <div class="column">
@@ -72,9 +76,12 @@
         <div>
           <p class="price-per-night">
             {{yacht.pricePerNight}} $
+          
             <br />
           </p>
           <span v-html="showStars"></span>
+          
+          <br />
           <small class="is-center is-clearfix has-text-grey">{{getNumberOfReviews}} Reviews</small>
           <router-link
             :to="getUrlWithYachtId"
@@ -208,7 +215,8 @@ a {
 
 .img-boat {
   /* max-width: 20vw; */
-  min-height: 220px;
+  min-height: 220px;  
+  max-height: 225px;
   margin-top: -1.7rem;
 }
 .img-owner {
@@ -218,7 +226,7 @@ a {
 }
 
 .img-wrap {
-  height: auto;
+  /* height: auto; */
   overflow: auto;
 }
 </style>
