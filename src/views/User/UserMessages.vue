6400526a873d56b4f986025bda85ba91ab49fc18
Itay Azulay <itay.responder@gmail.com>
2019-07-22 10:36:15 +0300

    update user owner msgs

--- src/views/User/UserMessages.vue
+++ src/views/User/UserMessages.vue
@@ -1,17 +1,19 @@
 <template>
   <section>
-      <h1>UserMessages</h1>
-    <h1 v-if="!this.userLoggedInReservations">You dont have any messages</h1>
+    <h1 v-if="!this.userLoggedInReservations[0]">
+      <b>You dont have any messages</b>
+    </h1>
     <nav v-else>
       <b>You have got new messages</b>
       <br />
       <!-- {{this.userLoggedInReservations}} -->
-    <user-messages-list v-if="this.userLoggedInReservations[0].yacht" :reservations="reservations" @replyUser="replyUser"></user-messages-list>
-    <div v-else>
-        {{this.userLoggedInReservations}}
-    </div>
+      <user-messages-list
+        v-if="this.loggedInUser.isOwner"
+        :reservations="reservations"
+        @replyUser="replyUser"
+      ></user-messages-list>
+      <div v-else>{{this.userLoggedInReservations}}</div>
     </nav>
-    <h2>Messages</h2>
   </section>
 </template>
 
@@ -21,32 +23,42 @@ import UserMessagesList from "@/components/user/UserMessagesList";
 export default {
   data() {
     return {
+      loggedInUser: null,
       userLoggedInReservations: []
     };
   },
-  created() {
-    this.userLoggedInReservations = this.$store.getters.userLoggedIn.reservations;
+  async created() {
+    this.loggedInUser = this.$store.getters.userLoggedIn;
+    try {
+        this.userLoggedInReservations = await this.$store.dispatch({type: "loadUserReservations", userId: this.loggedInUser._id})
+    } catch (err) {
+        console.log("Could not load user reservations error:", err)
+    }
   },
   computed: {
-      reservations() {
-          return this.$store.getters.userLoggedIn.reservations;
-      }
+    reservations() {
+      return this.$store.getters.userReservations;
+    }
   },
   methods: {
-      async replyUser(replyUser) {
-          try {
-              const answer = await this.$store.dispatch({type: "replyUser", replyUser})
-              await Swal.fire("Allright",
-            `Message has been sent to the user`,
-            "success"
-          );
-          } catch (err) {
-              console.log('UserMesseges eror', err)
-          }
+    async replyUser(replyUser) {
+      try {
+        const answer = await this.$store.dispatch({
+          type: "replyUser",
+          replyUser
+        });
+        await Swal.fire(
+          "Allright",
+          `Message has been sent to the user`,
+          "success"
+        );
+      } catch (err) {
+        console.log("UserMesseges eror", err);
       }
+    }
   },
   components: {
-      UserMessagesList
+    UserMessagesList
   }
 };
 </script>
