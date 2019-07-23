adf6f8e9fa4cd69404cf20b671d00c24cf5f46fb
nivb52 <nivb52@gmail.com>
2019-07-23 19:05:01 +0300

    responsive added

--- src/components/Citeis.vue
+++ src/components/Citeis.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="container" index="13" style="position: relative !important;">
+	<div class="container is-relative" index="13" >
 		<div class="margin-6rem">
 			<p class="title">
 				Sail Your Way
@@ -11,7 +11,7 @@
 			<div class="columns transition is-multiline">
 				<div v-for="(detail,idx) in locationDetails " :key="idx"
 				 class="column 
-				is-one-third-desktop is-one-tlocationImgshird-tablet">
+				is-one-third-desktop is-half-tablet is-one-mobile">
 					<OneCardItem :detail="detail" />
 				</div>
 			</div>
@@ -27,12 +27,12 @@ export default {
 	},
 	data() {
 		return {
-			locationDetails:[{img:'https://res.cloudinary.com/ocean-yachts/image/upload/v1563777458/Yacths/locations/4_tmjzp0.jpg',city:'Sydney,Australia', url:'australia'},
-			{img:'https://res.cloudinary.com/ocean-yachts/image/upload/v1563777463/Yacths/locations/5_b8dsvh.jpg',city:'San marino,italy', url:'italy'},
-			{img:'https://res.cloudinary.com/ocean-yachts/image/upload/v1563777457/Yacths/locations/7_qn4kau.jpg',city:'Bilbao,Spain', url:'spain'},
-			{img:'https://res.cloudinary.com/ocean-yachts/image/upload/v1563777456/Yacths/locations/1_myixgj.jpg',city:'Naples,Spain', url:'spain'},
-			{img:'https://res.cloudinary.com/ocean-yachts/image/upload/v1563777453/Yacths/locations/3_tnr0rn.jpg',city:'Nice,France', url:'france'},
-			{img:'https://res.cloudinary.com/ocean-yachts/image/upload/v1563777451/Yacths/locations/6_bq7tvi.jpg',city:'Crete,Greece', url:'greece'}],
+			locationDetails:[{img:'https://res.cloudinary.com/ocean-yachts/image/upload/v1563777458/Yacths/locations/4_tmjzp0.jpg',city:'Sydney',country: 'Australia', url:'australia'},
+			{img:'https://res.cloudinary.com/ocean-yachts/image/upload/v1563777463/Yacths/locations/5_b8dsvh.jpg',city:'San marino',country: 'italy', url:'italy'},
+			{img:'https://res.cloudinary.com/ocean-yachts/image/upload/v1563777457/Yacths/locations/7_qn4kau.jpg',city:'Bilbao',country: 'Spain', url:'spain'},
+			{img:'https://res.cloudinary.com/ocean-yachts/image/upload/v1563777456/Yacths/locations/1_myixgj.jpg',city:'Naples',country: 'Spain', url:'spain'},
+			{img:'https://res.cloudinary.com/ocean-yachts/image/upload/v1563777453/Yacths/locations/3_tnr0rn.jpg',city:'Nice',country: 'France', url:'france'},
+			{img:'https://res.cloudinary.com/ocean-yachts/image/upload/v1563777451/Yacths/locations/6_bq7tvi.jpg',city:'Crete',country: 'Greece', url:'greece'}],
 			// cities:['Sydney,Australia','San marino,italy','Barcelona,Spain','Nice,France','Crete,Greece','Bilbao,Spain']
 			// cities: "@/assets/img/loc/"
 		};
