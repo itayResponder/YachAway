08f170345b84c42a7f4f93dfdd5eab155788f9cb
nivb52 <nivb52@gmail.com>
2019-07-24 00:21:32 +0300

    fix image moving from center

--- src/components/home/FeatureArea.vue
+++ src/components/home/FeatureArea.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="hero-body mobile is-paddingless is-relative">
 		<!-- <img alt="" src="../assets/img/yacht-1280x720.jpg" style="width: 100%; height: 343px; object-fit: cover; object-position: left center;"> -->
-		<img alt="" src="https://res.cloudinary.com/ocean-yachts/image/upload/v1563779317/Yacths/optional%20for%20mobile/7_qewfrv.jpg" class="is-scale-down" style="width: 100%; height: 600px; object-position: left center;">
+		<img alt="" src="https://res.cloudinary.com/ocean-yachts/image/upload/v1563779317/Yacths/optional%20for%20mobile/7_qewfrv.jpg" class="is-cover" style="width: 100%; height: 600px; object-position: left center;">
 		<div class="hero-body">
 			<div class="container has-text-centered">
 				<p class="title has-text-white button is-primary is-medium is-capitalized is-relative"  style="top:-50vh; ">
