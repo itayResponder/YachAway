e829131fcad7d2383b44963f3baab68ea6ba98ff
Itay Azulay <itay.responder@gmail.com>
2019-10-06 11:33:36 +0300

    test

--- src/views/User/ProfileHome.vue
+++ src/views/User/ProfileHome.vue
@@ -2,9 +2,6 @@
   <main class="bookings">
     <div class="tabs is-nav is-taller scroll-h has-side-space">
       <ul class="width-limit">
-        <!-- <li class="">
-					<router-link to="/travel" class="is-active">Discover</router-link>
-        </li>-->
         <li class>
           <router-link to="/profile/messages" class>Inbox</router-link>
         </li>
@@ -19,15 +16,7 @@
         </li>
       </ul>
     </div>
-    <!-- <header class="width-limit has-space-h">
-			<div class="is-flex field">
-				<div class="is-clipped">
-					<p class="field t2 nowrap-ellipsis">My bookings</p>
-				</div>
-			</div>
-    </header>-->
     <section class="width-limit has-space-h is-fullwidth-when-narrow">
-      <!-- <div class="box all-bookings is-light is-paddingless is-collapsed-when-narrow has-fade"> -->
         <transition
           name="inner-router-anime"
           enter-active-class="routateIn"
@@ -35,7 +24,6 @@
         >
           <router-view />
         </transition>
-      <!-- </div> -->
     </section>
   </main>
 </template>
